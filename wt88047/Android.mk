a713dc14a4dcf4f4d12f6b737b5978833b3677e8
Ankan005 <Ankan005@gmail.com>
2018-09-04 13:35:43 +0000

    new

--- wt88047/Android.mk
+++ wt88047/Android.mk
@@ -32,6 +32,17 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := ims
+LOCAL_MODULE_OWNER := wingtech
+LOCAL_SRC_FILES := proprietary/app/ims/ims.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := imscmlibrary
 LOCAL_MODULE_OWNER := wingtech
 LOCAL_SRC_FILES := proprietary/vendor/framework/imscmlibrary.jar
